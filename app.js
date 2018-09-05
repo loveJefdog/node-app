170e7389a18b1c5e988e0f62e61be3b12df28649
gitloveJefdog <soccer_is_loved_by_me_0507@ezweb.ne.jp>
2018-09-05 02:36:34 +0000

    htmlの追加

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const http = require('http');
 
 let server = http.createServer(
     (req,res)=>{
-        res.end('Hello, node.js!');
+        res.end('<html><body><h1>Hello</h1><p>welcome to Node.js</p></body></html>');
     }
 );
 
 server.listen(8000);
+
