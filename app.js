27ecb5742f834172418b72a3529af7418dd6b56a
gitloveJefdog <soccer_is_loved_by_me_0507@ezweb.ne.jp>
2018-09-05 09:19:44 +0000

    ejsの追加

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 
 const http = require('http');
 const fs = require('fs');
+const ejs = require('ejs');
+
+const index_page=fs.readFileSync('./index.ejs','utf8')
 
 let server = http.createServer(getFromClient);
 
@@ -9,15 +12,10 @@ console.log('server start!!');
 
 
 function getFromClient(request,response){
-    fs.readFile('./index.html','UTF-8',
-        (error,data)=>{
-            var content = data.
-                replace(/dummy_title/g, 'タイトルです').
-                replace(/dummy_content/g, 'これがコンテンツです')
+    var content = ejs.render(index_page);
             response.writeHead(200,{'Content-Type': 'text/html'});
             response.write(content);
             response.end();
-        });
 };
 
 
