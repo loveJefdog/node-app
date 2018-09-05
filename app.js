1de003e2462d0d6a52b379545d6ba615a965a969
gitloveJefdog <soccer_is_loved_by_me_0507@ezweb.ne.jp>
2018-09-05 02:52:48 +0000

    header情報の追加

--- app.js
+++ app.js
@@ -3,9 +3,18 @@ const http = require('http');
 
 let server = http.createServer(
     (req,res)=>{
-        res.end('<html><body><h1>Hello</h1><p>welcome to Node.js</p></body></html>');
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<!DOCTYPE html><html lang="ja">');
+        res.write('<head><meta charset="UTF-8">');
+        res.write('<title>Hello</title></head>');
+        res.write('<body><h1>Hello</h1>');
+        res.write('<p>This is Node.js sample page.</p>');
+        res.write('<p>これは，Node.jsのサンプルページです．</p>');
+        res.write('</body></html>');
+        res.end();
     }
 );
 
 server.listen(8000);
+console.log('server start!!');
 
